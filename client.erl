3e41988794201bc89ce5ba6d0fde9f3890709e9f
yanqiu <1039954615@qq.com>
2025-10-01 17:18:51 +0200

    Feature: Change nick

--- client.erl
+++ client.erl
@@ -4,19 +4,19 @@
 % This record defines the structure of the state of a client.
 % Add whatever other fields you need.
 -record(client_st, {
-  gui, % atom of the GUI process
-  nick, % nick/username of the client
-  server % atom of the chat server
+	gui, % atom of the GUI process
+	nick, % nick/username of the client
+	server % atom of the chat server
 }).
 
 % Return an initial state record. This is called from GUI.
 % Do not change the signature of this function.
 initial_state(Nick, GUIAtom, ServerAtom) ->
-  #client_st{
-    gui = GUIAtom,
-    nick = Nick,
-    server = ServerAtom
-  }.
+	#client_st{
+		gui = GUIAtom,
+		nick = Nick,
+		server = ServerAtom
+	}.
 
 % handle/2 handles each kind of request from GUI
 % Parameters:
@@ -28,58 +28,78 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
 
 % Join channel
 handle(St, {join, Channel}) ->
-  % join user to channel
-  case catch (genserver:request(St#client_st.server, {join, Channel, self()})) of
-    {'EXIT', _} ->
-      {reply, {error, server_not_reached, "server unreachable"}, St};    ok ->
-    {reply, ok, St};
-    error ->
-      {reply, {error, user_already_joined, "user already joined"}, St}
-  end;
+	% Attempt to join the user to the specified channel by sending a request to the server
+	case catch (genserver:request(St#client_st.server, {join, Channel, self()})) of
+		% If the server process crashes or is unreachable, catch the EXIT signal
+		{'EXIT', _} ->
+			{reply, {error, server_not_reached, "server unreachable"}, St};
+		% If the server responds with 'ok', the join operation was successful
+		ok ->
+			{reply, ok, St};
+		% If the server responds with 'error', the user is already in the channel
+		error ->
+			{reply, {error, user_already_joined, "user already joined"}, St}
+	end;
 
 % Leave channel
 handle(St, {leave, Channel}) ->
-  case catch (genserver:request(list_to_atom(Channel), {leave, self()})) of
-    {'EXIT', _} ->
-      {reply, {error, server_not_reached, "server unreachable"}, St};
-    ok ->
-      {reply, ok, St};
-    error ->
-      {reply, {error, user_not_joined, "user not in channel"}, St}
-  end;
+	% Send a leave request directly to the channel process
+	% Convert the channel name from string to atom to get the process identifier
+	% Self() is the Pid of this client process
+	case catch (genserver:request(list_to_atom(Channel), {leave, self()})) of
+		{'EXIT', _} ->
+			{reply, {error, server_not_reached, "server unreachable"}, St};
+		ok ->
+			{reply, ok, St};
+		error ->
+			{reply, {error, user_not_joined, "user not in channel"}, St}
+	end;
 
 % Sending message (from GUI, to channel)
+% GUI -> Client process(this code) -> Channel process(receive message) -> Other clients in channel(broadcast to all members)
 handle(St, {message_send, Channel, Msg}) ->
-  case (catch genserver:request(list_to_atom(Channel), {message_send, St#client_st.nick, Msg, self()})) of
-    {'EXIT', _} ->
-      {reply, {error, server_not_reached, "server unreachable"}, St};
-    message_send -> {reply, ok, St};
-    error -> {reply, {error, user_not_joined, "user not in channel"}, St}
-  end;
+	% Send a message to the specified channel
+	% The request includes the sender's nickname, message content, and PID
+	case (catch genserver:request(list_to_atom(Channel), {message_send, St#client_st.nick, Msg, self()})) of
+		{'EXIT', _} ->
+			{reply, {error, server_not_reached, "server unreachable"}, St};
+		ok ->
+			{reply, ok, St};
+		error ->
+			{reply, {error, user_not_joined, "user not in channel"}, St}
+	end;
 
 % This case is only relevant for the distinction assignment!
 % Change nick (no check, local only)
 handle(St, {nick, NewNick}) ->
-  {reply, ok, St#client_st{nick = NewNick}};
-
+	% Send a request to the server to change the nickname
+	% Include both the old nick and the new nick for validation
+	case catch (genserver:request(St#client_st.server, {nick, St#client_st.nick, NewNick})) of
+		{'EXIT', _} ->
+			{reply, {error, server_not_reached, "Server does not respond"}, St};
+		ok ->
+			{reply, ok, St#client_st{nick = NewNick}};
+		error ->
+			{reply, {error, nick_taken, "Nick is taken"}, St}
+	end;
 % ---------------------------------------------------------------------------
 % The cases below do not need to be changed...
 % But you should understand how they work!
 
 % Get current nick
 handle(St, whoami) ->
-  {reply, St#client_st.nick, St};
+	{reply, St#client_st.nick, St};
 
 % Incoming message (from channel, to GUI)
 handle(St = #client_st{gui = GUI}, {message_receive, Channel, Nick, Msg}) ->
-  gen_server:call(GUI, {message_receive, Channel, Nick ++ "> " ++ Msg}),
-  {reply, ok, St};
+	gen_server:call(GUI, {message_receive, Channel, Nick ++ "> " ++ Msg}),
+	{reply, ok, St};
 
 % Quit client via GUI
 handle(St, quit) ->
-  % Any cleanup should happen here, but this is optional
-  {reply, ok, St};
+	% Any cleanup should happen here, but this is optional
+	{reply, ok, St};
 
 % Catch-all for any unhandled requests
 handle(St, Data) ->
-  {reply, {error, not_implemented, "Client does not handle this command"}, St}.
+	{reply, {error, not_implemented, "Client does not handle this command"}, St}.
--- server.erl
+++ server.erl
@@ -1,85 +1,106 @@
 -module(server).
 -export([start/1, stop/1]).
 
+% This record defines the structure of the state of a server
 -record(server_st, {
-  name,
-  channels
+	name,      % atom of the server process
+	channels,  % list of channels
+	nicks      % list of all user nicknames
 }).
 
+% This record defines the structure of the state of a channel
 -record(channel_st, {
-  name,
-  clients
+	name,      % atom of the channel process
+	clients    % list of client PIDs in this channel
 }).
 
+% Initialize server state
 initial_server_state(ServerAtom) ->
-  #server_st{
-    name = ServerAtom,
-    channels = []
-  }.
+	#server_st{
+		name = ServerAtom,
+		channels = [],
+		nicks = []
+	}.
 
+% Initialize channel state
 initial_channel_state(ChannelAtom) ->
-  #channel_st{
-    name = ChannelAtom,
-    clients = []
-  }.
+	#channel_st{
+		name = ChannelAtom,
+		clients = []
+	}.
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-  genserver:start(ServerAtom, initial_server_state(ServerAtom), fun handleS/2).
+	genserver:start(ServerAtom, initial_server_state(ServerAtom), fun handle/2).
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-  genserver:request(ServerAtom, {stop}),
-  genserver:stop(ServerAtom).
+	genserver:request(ServerAtom, {stop}),
+	genserver:stop(ServerAtom).
 
-handleS(St, {stop}) ->
-  lists:foreach(fun(Channel) -> genserver:stop(list_to_atom(Channel)) end, St#server_st.channels),
-  {reply, ok, St};
+% Stop the server and all associated channel processes
+handle(St, {stop}) ->
+	lists:foreach(fun(Channel) -> genserver:stop(list_to_atom(Channel)) end, St#server_st.channels),
+	{reply, ok, St};
 
+% Handle a user joining a channel - creates channel if it doesn't exist
+handle(St, {join, Channel, Pid}) ->
+	case whereis(list_to_atom(Channel)) of
+		undefined -> % true if no such channel
+			genserver:start(list_to_atom(Channel), initial_channel_state(Channel), fun handleChannel/2);
+		_ -> % channel exists
+			ok
+	end,
 
+	case catch (genserver:request(list_to_atom(Channel), {join, Pid})) of
+		ok -> % could join
+			{reply, ok, St#server_st{channels = [Channel | St#server_st.channels]}};
+		_ ->
+			{reply, error, St}
+	end;
 
-handleS(St, {join, Channel, Pid}) ->
-  case whereis(list_to_atom(Channel)) of
-    undefined -> % true if no such channel
-      genserver:start(list_to_atom(Channel), initial_channel_state(Channel), fun handleC/2);
-    _ -> % channel exists
-      ok
-  end,
+% Handle nickname change request - checks if new nick is available
+handle(St, {nick, OldNick, NewNick}) ->
+	% Extract the existing nicks list from server state
+	ExistingNicks = St#server_st.nicks,
+	% Check if the new nickname already exists
+	case lists:member(NewNick, ExistingNicks) of
+		true ->
+			{reply, error, St};
+		false ->
+			% Update state: remove old nick and add new nick
+			NewState = St#server_st{nicks = [NewNick | lists:delete(OldNick, ExistingNicks)]},
+			{reply, nick, NewState}
+	end.
 
-  case catch (genserver:request(list_to_atom(Channel), {join, Pid})) of
-    ok -> % could join
-      {reply, ok, St#server_st{
-        channels = [Channel | St#server_st.channels]}};
-    _ ->
-      {reply, error, St}
-  end.
+% Add a user to the channel's client list
+handleChannel(St, {join, Pid}) ->
+	case lists:member(Pid, St#channel_st.clients) of
+		false -> % user not found
+			{reply, ok, St#channel_st{clients = [Pid | St#channel_st.clients]}}; % add user
+		{'EXIT', _} -> % user is already in channel
+			{reply, error, St}
+	end;
 
-handleC(St, {join, Pid}) ->
-  case lists:member(Pid, St#channel_st.clients) of
-    false -> % user not found
-      {reply, ok, St#channel_st{clients = [Pid | St#channel_st.clients]}}; % add user
-    {'EXIT', _} -> % user is already in channel
-      {reply, error, St}
-  end;
+% Remove a user from the channel's client list
+handleChannel(St, {leave, Pid}) ->
+	case lists:member(Pid, St#channel_st.clients) of
+		true -> % user found
+			{reply, ok, St#channel_st{clients = lists:delete(Pid, St#channel_st.clients)}}; %delete user
+		_ -> % no user found
+			{reply, error, St}
+	end;
 
-handleC(St, {leave, Pid}) ->
-  case lists:member(Pid, St#channel_st.clients) of
-    true -> % user found
-      {reply, ok, St#channel_st{clients = lists:delete(Pid, St#channel_st.clients)}}; %delete user
-    _ -> % no user found
-      {reply, error, St}
-  end;
-
-
-handleC(St, {message_send, Pid, Msg, Sender}) ->
-  case lists:member(Sender, St#channel_st.clients) of
-    true -> % user in channel
-      spawn(fun() ->
-        [genserver:request(ChannelUsers, {message_receive, St#channel_st.name, Pid, Msg}) || ChannelUsers <- St#channel_st.clients, ChannelUsers =/= Sender]
-            end),
-      {reply, message_send, St};
-    false -> % user not in channel
-      {reply, error, St}
-  end.
+% Broadcast a message to all users in the channel except the sender
+handleChannel(St, {message_send, Pid, Msg, Sender}) ->
+	case lists:member(Sender, St#channel_st.clients) of
+		true -> % user in channel
+			spawn(fun() ->
+				[genserver:request(ChannelUsers, {message_receive, St#channel_st.name, Pid, Msg}) || ChannelUsers <- St#channel_st.clients, ChannelUsers =/= Sender]
+				  end),
+			{reply, ok, St};
+		false -> % user not in channel
+			{reply, error, St}
+	end.
